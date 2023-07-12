df7473a1fbaf88b487e78c6520d7d9deba2683cc
Rummut <kzantip@gmail.com>
2023-07-12 23:53:12 +0300

    add pagination

--- src/index.js
+++ src/index.js
@@ -1,9 +1,44 @@
 import PixabiAPI from './pixabi_api';
+import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.css';
 import { getImagesData } from './images/gallery_images';
 import Notiflix from 'notiflix';
 
+
+
 const formEl = document.querySelector('#search-form');
 const galleryEl = document.querySelector('.gallery');
+
+const options = {
+  totalItems: 0,
+  itemsPerPage: 40,
+  visiblePages: 5,
+  page: 1,
+  // centerAlign: false,
+  // firstItemClassName: 'tui-first-child',
+  // lastItemClassName: 'tui-last-child',
+  // template: {
+  //   page: '<a href="#" class="tui-page-btn">{{page}}</a>',
+  //   currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+  //   moveButton:
+  //     '<a href="#" class="tui-page-btn tui-{{type}}">' +
+  //       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+  //     '</a>',
+  //   disabledMoveButton:
+  //     '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+  //       '<span class="tui-ico-{{type}}">{{type}}</span>' +
+  //     '</span>',
+  //   moreButton:
+  //     '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
+  //       '<span class="tui-ico-ellip">...</span>' +
+  //     '</a>'
+  // }
+};
+
+const container = document.getElementById('tui-pagination-container');
+const instance = new Pagination(container, options);
+
+const page = instance.getCurrentPage()
 
 const getRequest = new PixabiAPI();
 
@@ -26,12 +61,18 @@ formEl.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
+ 
   const inputValue = formEl.elements.searchQuery.value.trim();
-  getRequest.q = inputValue;
+  getRequest.query = inputValue;
 
+  if (inputValue === "") {
+   return   Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.')
+  } else { 
   async function getImagesInputValue() {
-    try {
-      const { hits } = await getRequest.getImagesInput(1);
+    try { 
+      const {total, hits } = await getRequest.getImagesInput(page);
+      instance.reset(total)
       const markup = getImagesData(hits);
       galleryEl.innerHTML = '';
       galleryEl.insertAdjacentHTML('afterbegin', markup);
@@ -41,6 +82,22 @@ function onSubmit(e) {
       );
     }
   }
-
   getImagesInputValue();
 }
+}
+
+async function getImagesByPagination(event) {
+  const currentPage = event.page
+  try { 
+      const { hits } = await getRequest.getImagesInput(currentPage);
+      const markup = getImagesData(hits);
+      galleryEl.innerHTML = '';
+      galleryEl.insertAdjacentHTML('afterbegin', markup);
+    } catch (error) {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+}
+
+instance.on('afterMove', getImagesByPagination)
--- src/pixabi_api.js
+++ src/pixabi_api.js
@@ -4,28 +4,28 @@ import Notiflix from 'notiflix';
 class PixabiAPI {
   #BASE_URL = 'https://pixabay.com/api/';
   #API_KEY = '38182607-aab9c53a0cd2b78abfbdb0e24';
-  #q = '';
-//   async getPhotoAll(page) {
-//     try {
-//       const response = await axios.get(
-//         `${this.#BASE_URL}?key=${
-//           this.#API_KEY
-//         }&q=&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
-//       );
-//       return response.data;
-//     } catch (error) {
-//       Notiflix.Notify.failure(
-//         'Sorry, there are no images matching your search query. Please try again.'
-//       );
-//     }
-//   }
+  #query = '';
+  // async getPhotoAll(page) {
+  //   try {
+  //     const response = await axios.get(
+  //       `${this.#BASE_URL}?key=${
+  //         this.#API_KEY
+  //       }&q=popular&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+  //     );
+  //     return response.data;
+  //   } catch (error) {
+  //     Notiflix.Notify.failure(
+  //       'Sorry, there are no images matching your search query. Please try again.'
+  //     );
+  //   }
+  // }
 
   async getImagesInput(page) {
     try {
       const response = await axios.get(
         `${this.#BASE_URL}?key=${this.#API_KEY}&q=${
-          this.#q
-        }&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`
+          this.#query
+        }&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
       );
       return response.data;
     } catch (error) {
@@ -35,8 +35,8 @@ class PixabiAPI {
     }
   }
 
-  set q(newInput) {
-    this.#q = newInput;
+  set query(newInput) {
+    this.#query = newInput;
   }
 }
 
